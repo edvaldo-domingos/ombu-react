582a190bca4a0cfd7ccf71c132ec0f259c0e35b1
edvaldo <brungo1995@gmail.com>
2022-03-23 19:03:49 +0200

    feature:(General) - added like and deslike functionality

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,14 +1,43 @@
 import { useEffect } from "react";
 import HomeVM from "./HomeVM";
+import styled from "styled-components";
+
+const StyledContainer = styled.div`
+  margin-top: 20px;
+`;
+
+const StyledRow = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledRCol = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 function Home() {
-  const { joke, fetchRandomJoke } = HomeVM();
+  const {
+    joke,
+    fetchRandomJoke,
+    likedJokes,
+    onClickLikeJoke,
+    onClickDeslikeJoke,
+  } = HomeVM();
+
   useEffect(() => {
     fetchRandomJoke();
   }, []);
 
-  console.log(joke);
-  return <div style={{ marginTop: "100px" }}></div>;
+  return (
+    <StyledContainer>
+      <StyledRow>{joke?.description}</StyledRow>
+      <StyledRow>
+        <button onClick={onClickLikeJoke}>Like</button>{" "}
+        <button onClick={onClickDeslikeJoke}>Dislike</button>{" "}
+      </StyledRow>
+    </StyledContainer>
+  );
 }
 
 export default Home;
--- src/pages/home/HomeVM.jsx
+++ src/pages/home/HomeVM.jsx
@@ -3,15 +3,28 @@ import { BASE_URL } from "../../utils/constants";
 
 function HomeVM() {
   const [error, setError] = useState(null);
-  const [joke, setJoke] = useState(null);
+  const [joke, setJoke] = useState({});
+  const [likedJokes, setLikedJokes] = useState({});
+  const [deslikedJokes, setDeslikedJokes] = useState({});
+  const [numOdRetries, setNumOdRetries] = useState(0);
+
+  //   category: "Spooky"
+  //   delivery: "So they can keep their ghoulish figures."
+  //   error: false
+  //   flags: {nsfw: false, religious: false, political: false, racist: false, sexist: false, …}
+  //   id: 295
+  //   lang: "en"
+  //   safe: true
+  //   setup: "Why do ghosts go on diets?"
+  //   type: "twopart"
 
   const mapJokeResponseFromApi = (joke) => {
     switch (joke.type) {
       case "twopart":
-        setJoke(`${joke.setup}, ${joke.delivery}`);
+        setJoke({ ...joke, description: `${joke.setup}, ${joke.delivery}` });
         break;
       default:
-        setJoke(`${joke.joke}`);
+        setJoke({ ...joke, description: joke.joke });
         break;
     }
   };
@@ -20,15 +33,42 @@ function HomeVM() {
     try {
       const response = await fetch(BASE_URL);
       const data = await response.json();
+
       mapJokeResponseFromApi(data);
     } catch (error) {
       console.log(error);
     }
   };
 
+  const onClickLikeJoke = async () => {
+    await fetchRandomJoke();
+
+    while (numOdRetries < 5 && likedJokes[joke.id]) {
+      await fetchRandomJoke();
+    }
+
+    setNumOdRetries(0);
+    setLikedJokes({ ...likedJokes, [joke.id]: joke.id });
+  };
+
+  const onClickDeslikeJoke = async () => {
+    await fetchRandomJoke();
+
+    while (numOdRetries < 5 && deslikedJokes[joke.id]) {
+      await fetchRandomJoke();
+    }
+
+    setNumOdRetries(0);
+    setLikedJokes({ ...likedJokes, [joke.id]: joke.id });
+  };
+
   return {
     fetchRandomJoke,
     joke,
+    likedJokes,
+    setLikedJokes,
+    onClickLikeJoke,
+    onClickDeslikeJoke,
   };
 }
 
